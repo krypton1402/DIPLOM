b705af719b6b407906737c2a81e8e7ebf6ca9b68
krypton1402 <artemiy.akberov@yandex.ru>
2022-08-23 14:04:43 +0300

    бавил фоновое изображение. Следующий шаг пытаюсь добавить часы

--- Test/Test/Views/MainWindow.axaml.cs
+++ Test/Test/Views/MainWindow.axaml.cs
@@ -1,13 +1,24 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Threading;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using Test.MyControls;
 namespace Test.Views
 {
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
+
         }
     }
+       
 }
