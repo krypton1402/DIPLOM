abef5692a1dcdfb3aa9b6366ec08d1b404524a3f
krypton1402 <artemiy.akberov@yandex.ru>
2022-08-24 13:04:23 +0300

    Добавил дату и время, дальше строки состояний

--- Test/Test/Views/MainWindow.axaml.cs
+++ Test/Test/Views/MainWindow.axaml.cs
@@ -17,8 +17,19 @@ namespace Test.Views
         public MainWindow()
         {
             InitializeComponent();
-
+            DispatcherTimer LiveTime = new DispatcherTimer();
+            LiveTime.Interval = TimeSpan.FromSeconds(1);
+            LiveTime.Tick += timer_Tick;
+            LiveTime.Start();
         }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            LiveTimeLabel.Content = DateTime.Now.ToString("HH:mm:ss");
+            LiveDateLabel.Content = DateTime.Now.ToString("dd/MM/yyyy");
+        }
+       
+
     }
        
 }
